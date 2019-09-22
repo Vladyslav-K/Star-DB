de6b34a708bb4e4f073126709ed6f328122fcf4c
Vladyslav-K <your.reliable.man@gmail.com>
2019-09-22 23:52:04 +0300

    I learned lifecycle hooks in React JS

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div class="lds-css ng-scope">
-      <div class="lds-double-ring">
+    <div className="lds-css ng-scope">
+      <div className="lds-double-ring">
         <div></div>
         <div></div>
       </div>
